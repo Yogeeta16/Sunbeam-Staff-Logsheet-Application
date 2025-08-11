2e83e5400b5f98ecc3bc548a68deb5c53d651a71
Yogeeta16 <bsyog92@gmail.com>
2025-08-11 08:28:28 +0530

    backend login signup started

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,18 @@
 // db.js
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+       database: process.env.DB_NAME
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('MySQL Connection Error:', err);
+        return;
+    }
+    console.log('✅ MySQL Connected');
+});
+
+module.exports = db;
--- backend/models/Staff.js
+++ backend/models/Staff.js
@@ -1,1 +1,15 @@
 // Staff.js
+const db = require('../config/db');
+
+exports.findByEmail = async (email) => {
+    const [rows] = await db.query("SELECT * FROM users WHERE email = ?", [email]);
+    return rows[0];
+};
+
+exports.createUser = async (name, email, hashedPassword, role) => {
+    const [result] = await db.query(
+        "INSERT INTO users (name, email, password, role) VALUES (?, ?, ?, ?)",
+        [name, email, hashedPassword, role]
+    );
+    return result.insertId;
+};
