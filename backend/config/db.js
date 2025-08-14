0ed6ad0dff16b20188de43344c9c3e0ef81268d6
Yogeeta16 <bsyog92@gmail.com>
2025-08-14 09:40:29 +0530

    backend login signup done

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,22 @@
-// db.js
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-       database: process.env.DB_NAME
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-db.connect((err) => {
+pool.getConnection((err, connection) => {
     if (err) {
-        console.error('MySQL Connection Error:', err);
+        console.error('❌ MySQL Connection Error:', err);
         return;
     }
     console.log('✅ MySQL Connected');
+    connection.release();
 });
 
-module.exports = db;
+module.exports = pool.promise(); 
